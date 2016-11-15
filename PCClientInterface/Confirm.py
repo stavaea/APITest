1c1f0da6ad2ba9f149f877bf64aab9ac0b62516c
lishenghong <lishenghong@talkweb.com.cn>
2016-11-15 11:51:29 +0800

    updated

---
+++ PCClientInterface/Confirm.py
@@ -0,0 +1,23 @@
+#-*- coding:UTF-8 -*-
+
+'''
+Created on 2016年9月26日
+
+@author: lsh
+'''
+
+def objIsInList(obj,compareList):
+    if obj in compareList:
+        return True
+    
+    else:
+        return False
+
+
+def VerifyDataStucture(Expect,actual):
+    for key in actual:
+        if key not in Expect:
+            return False
+    return True     
+                
+
