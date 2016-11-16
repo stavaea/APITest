0206e537d4c378d1bb8cdcabef23c06f21b53f01
lishenghong <lishenghong@talkweb.com.cn>
2016-11-16 11:13:32 +0800

    update

---
+++ Workspace/GN100-selenium/src/Configuration.py
@@ -0,0 +1,11 @@
+'''
+Created on 2016Äê11ÔÂ16ÈÕ
+
+@author: lsh
+'''
+BaseUrl = "http://lshc.gn100.com"
+DriverName = "chrome"
+
+#µÇÂ¼
+uname = "18500643574"
+password = "111111"
---
+++ Workspace/GN100-selenium/src/Prerequirement/Initialize.py
@@ -0,0 +1,9 @@
+#-*- coidng:utf-8 -*-
+'''
+Created on 2016Äê11ÔÂ16ÈÕ
+
+@author: lsh
+'''
+
+if __name__ == '__main__':
+    pass
