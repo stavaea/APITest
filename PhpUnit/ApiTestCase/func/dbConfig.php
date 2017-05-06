3c1ea4d2dbe6e57a9532d1b7fef360ce890f7642
zhangjunbao <zhangjunbao@yunke.com>
2017-05-06 11:23:14 +0800

    modify test env db IP

--- PhpUnit/ApiTestCase/func/dbConfig.php
+++ PhpUnit/ApiTestCase/func/dbConfig.php
@@ -1,5 +1,7 @@
 <?php
-$mysql_server_name='115.28.222.160';
-$mysql_user='root';
+$mysql_server_name='121.42.232.104';
+$mysql_user='michael';
+$mysql_user_password='michael';
+$mysql_user='michael';
 global $IP;
-$IP='115.28.222.160';
+$IP='121.42.232.204';
