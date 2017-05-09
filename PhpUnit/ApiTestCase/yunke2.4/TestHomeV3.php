d635117e1ffb7ff7bf0d85cf96363de7c656b1bc
yangmingjuan <yangmingjuan@talkweb.com.cn>
2017-05-09 11:55:02 +0800

    云课2.6  小沃1.5

--- PhpUnit/ApiTestCase/yunke2.4/TestHomeV3.php
+++ PhpUnit/ApiTestCase/yunke2.4/TestHomeV3.php
@@ -23,7 +23,7 @@ class TestHomeV3 extends PHPUnit_Framework_TestCase
         $postdata['time']=strtotime(date('Y-m-d H:i:s'));
         $postdata['u']=self::$u;
         $postdata['v']=self::$v;
-        $postdata['params']['uid']= "23339";
+        $postdata['params']['uid']= "3596";
         $postdata['params']['condition']= "1,7,29";
         $key=interface_func::GetAppKey($postdata);
         $postdata['key']=$key;
@@ -33,13 +33,8 @@ class TestHomeV3 extends PHPUnit_Framework_TestCase
         $this->assertArrayHasKey('banner', $result['result']);
         $this->assertEquals('3', count($result['result']['subject']));
         $this->assertEquals('直播课', $result['result']['types'][0]['name']);
-        $this->assertEquals('4000', $result['result']['types'][0]['condition']['sort']);
         $this->assertEquals('录播课', $result['result']['types'][1]['name']);
         $this->assertEquals('免费课', $result['result']['types'][2]['name']);
-        $this->assertEquals('精品课', $result['result']['types'][3]['name']);
-        $this->assertEquals('http://test.gn100.com/assets_v2/interface/yunke/img/i/04.png?ver=132', $result['result']['types'][3]['img']);
-        $this->assertEquals('2000', $result['result']['types'][3]['condition']['feeType']);//付费
-        //$this->assertEquals('4000', $result['result']['types'][3]['condition']['sort']);//最新
         $this->assertArrayHasKey('userWillPlan', $result['result']);
         $this->assertArrayHasKey('courseName', $result['result']['trySeeList'][0]['list'][0]);
         $this->assertArrayHasKey('stime', $result['result']['trySeeList'][0]['list'][0]);
@@ -51,9 +46,8 @@ class TestHomeV3 extends PHPUnit_Framework_TestCase
                    $this->assertGreaterThanOrEqual(strtotime($result['result']['trySeeList'][$i+1]['time']), strtotime($result['result']['trySeeList'][$i]['time']));
             }
         } 
-        //$this->assertArrayHasKey('courseName', $result['result']['courseList'][0]);
-        //$this->assertArrayHasKey('subname', $result['result']['courseList'][0]);
-        $this->assertLessThanOrEqual('4', count($result['result']['courseList']));
+        $this->assertArrayHasKey('courseName', $result['result']['courseList'][0]);
+        $this->assertArrayHasKey('subname', $result['result']['courseList'][0]);
         if(count($result['result']['courseList'])!=0)
         {
             $this->assertArrayHasKey('courseName', $result['result']['courseList'][0]);
@@ -64,27 +58,8 @@ class TestHomeV3 extends PHPUnit_Framework_TestCase
             }
         }
         
-        $this->assertLessThanOrEqual('10', count($result['result']['teacherList']));
-        if(count($result['result']['teacherList'])!=0)
-        {
-            $this->assertArrayHasKey('teacherName', $result['result']['teacherList'][0]);
-            $this->assertArrayHasKey('teahcerImg', $result['result']['teacherList'][0]);
-            $this->assertArrayHasKey('totalTime', $result['result']['teacherList'][0]);
-            $this->assertArrayHasKey('comment', $result['result']['teacherList'][0]);
-            $this->assertArrayHasKey('provinceName', $result['result']['teacherList'][0]);
-            $this->assertArrayHasKey('subject', $result['result']['teacherList'][0]);
-        }
         
-        $this->assertGreaterThanOrEqual('2', count($result['result']['orgList']));
-        $this->assertEquals('1', $result['result']['orgList']['isMore']);
-        if(count($result['result']['orgList'])!=0)
-        {
-            $this->assertArrayHasKey('orgName', $result['result']['orgList']['list'][0]);
-            $this->assertArrayHasKey('orgImg', $result['result']['orgList']['list'][0]);
-            $this->assertArrayHasKey('desc', $result['result']['orgList']['list'][0]);
-            $this->assertArrayHasKey('courseNum', $result['result']['orgList']['list'][0]);
-            $this->assertArrayHasKey('teacherNum', $result['result']['orgList']['list'][0]);
-        }
+        
     }
     
     
