b96aa0018801686921359d94f21f39bbe7418dae
zhangjunbao <zhangjunbao@yunke.com>
2017-08-10 14:44:18 +0800

    update PC-interface config

--- Workspace/PC-interface/src/TestCases/Configuration.py
+++ Workspace/PC-interface/src/TestCases/Configuration.py
@@ -2,24 +2,23 @@
 #!/usr/bin/env python3
 
 salt = "gn1002015"
-HostUrl = "http://pingtai.gn100.com"
+HostUrl = "http://test.gn100.com"
 
 #db 建立连接信息
-db_host="116.25.222.160"
-db_user="michael"
-db_passwd = "michael"
+db_host="121.42.232.104"
+db_user="root"
 db_name = "db_course"
 
 #教师用户名和密码
-mobile="13641174151"
+mobile="13810236430"
 password= "111111"
 
 #开始上课/继续上课PlanID
-Plan_Id= 3559 
+Plan_Id= 3304 
 
 #学生列表
-course_id = 780 #课程Id
-class_id  = 914 #班级Id
+course_id = 153 #课程Id
+class_id  = 192 #班级Id
 
 #助教课程
 assitant_course= 11223
