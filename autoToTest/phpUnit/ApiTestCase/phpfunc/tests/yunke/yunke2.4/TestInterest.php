34690584cf19445c35cdc08510b3c9d188b10876
zhangjunbao <zhangjunbao@yunke.com>
2017-08-24 14:44:36 +0800

    update

--- autoToTest/phpUnit/ApiTestCase/phpfunc/tests/yunke/yunke2.4/TestInterest.php
+++ autoToTest/phpUnit/ApiTestCase/phpfunc/tests/yunke/yunke2.4/TestInterest.php
@@ -19,12 +19,12 @@ class TestInterest extends PHPUnit_Framework_TestCase
         ];
         $result = interfaceFunc::getPostData($this->url, $this->postData);
         $this->assertEquals('学前', $result['result'][0]['data'][0]['name']);
-        $this->assertEquals('2', count($result['result'][0]['data'][0]['data']));
+        $this->assertEquals('6', count($result['result'][0]['data'][0]['id']));
         $this->assertEquals('小学', $result['result'][0]['data'][1]['name']);
-        $this->assertEquals('5', count($result['result'][0]['data'][1]['data']));
+        $this->assertEquals('7', count($result['result'][0]['data'][1]['id']));
         $this->assertEquals('初中', $result['result'][0]['data'][2]['name']);
-        $this->assertEquals('3', count($result['result'][0]['data'][2]['data']));
+        $this->assertEquals('8', count($result['result'][0]['data'][2]['id']));
         $this->assertEquals('高中', $result['result'][0]['data'][3]['name']);
-        $this->assertEquals('2', count($result['result'][0]['data'][3]['data']));
+        $this->assertEquals('9', count($result['result'][0]['data'][3]['id']));
     }
 }
