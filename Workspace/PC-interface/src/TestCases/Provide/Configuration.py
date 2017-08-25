f7757c56b7ea52ec59d6c6e8eaec948427a6251b
zhangjunbao <zhangjunbao@yunke.com>
2017-08-25 11:49:44 +0800

    update

---
+++ Workspace/PC-interface/src/TestCases/Provide/Configuration.py
@@ -0,0 +1,25 @@
+# -*- coding:UTF-8 -*-
+#!/usr/bin/env python3
+
+salt = "gn1002015"
+HostUrl = "http://test.gn100.com"
+
+#db 建立连接信息
+db_host="121.42.232.104"
+db_user="root"
+db_name = "db_course"
+
+#教师用户名和密码
+mobile="13810236430"
+password= "111111"
+
+#开始上课/继续上课PlanID
+Plan_Id= 3304 
+
+#学生列表
+course_id = 153 #课程Id
+class_id  = 192 #班级Id
+
+#助教课程
+assitant_course= 11223
+assitant_course_class = 112
--- autoToTest/phpUnit/ApiTestCase/phpfunc/src/dbConnect.php
+++ autoToTest/phpUnit/ApiTestCase/phpfunc/src/dbConnect.php
@@ -11,7 +11,7 @@ class dbConnect
         $mysql_server_name='121.42.232.104';
         $mysql_user='michael';
         $mysql_user_password='michael';
-        $conn=mysqli_connect($mysql_server_name,$mysql_user,$mysql_user_password,$mysql_db,'3306') or die ("Failed to connect mysql");
+        $conn=mysqli_connect($mysql_server_name,$mysql_user,$mysql_user_password,$mysql_db,'3307') or die ("Failed to connect mysql");
         $conn->query("set names utf8");
         $resultQuery=mysqli_query($conn,$sql);
         if (is_bool($resultQuery)){
